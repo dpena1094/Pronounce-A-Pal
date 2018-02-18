93f213d8ba58bcbb6ddf0fada0e58f4f640b5c8d
Danny <dpena1094@gmail.com>
2018-02-18 10:19:41 -0800

    .

--- app/src/main/java/edu/csun/team5/pronounce_a_pal/RecordInitial2Activity.java
+++ app/src/main/java/edu/csun/team5/pronounce_a_pal/RecordInitial2Activity.java
@@ -7,18 +7,18 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class RecordInitial3Activity extends AppCompatActivity {
+public class RecordInitial2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_record_initial4);
+        setContentView(R.layout.activity_record_initial2);
 
         final Button startSpeechButton = findViewById(R.id.toggleButtonRecordInitial4Button);
 
         startSpeechButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(RecordInitial4Activity.this, RecordInitial5Activity.class);//view.getContext(), TrainingActivity.class);
+                Intent intent = new Intent(RecordInitial2Activity.this, RecordInitial5Activity.class);//view.getContext(), TrainingActivity.class);
                 startActivity(intent);
                 finish();
             }
