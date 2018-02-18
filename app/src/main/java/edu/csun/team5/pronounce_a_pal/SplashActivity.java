ffd1f3f1cfae7fe30d8ecbf64290bbe2236ed1f6
Danny <dpena1094@gmail.com>
2018-02-17 19:17:54 -0800

    .

--- app/src/main/java/edu/csun/team5/pronounce_a_pal/SplashActivity.java
+++ app/src/main/java/edu/csun/team5/pronounce_a_pal/SplashActivity.java
@@ -11,13 +11,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        SystemClock.sleep(2000);
 
         Intent intent = new Intent(this, LandingActivity.class);
         startActivity(intent);
