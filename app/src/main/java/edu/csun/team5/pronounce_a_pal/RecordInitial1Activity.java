4488b87ce953b508e4ab04f200ea9bc953101531
Danny <dpena1094@gmail.com>
2018-02-18 10:23:46 -0800

    .

--- app/src/main/java/edu/csun/team5/pronounce_a_pal/RecordInitial1Activity.java
+++ app/src/main/java/edu/csun/team5/pronounce_a_pal/RecordInitial1Activity.java
@@ -1,7 +1,10 @@
 package edu.csun.team5.pronounce_a_pal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordInitial1Activity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class RecordInitial1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_initial1);
+
+        final Button continueButton = findViewById(R.id.toggleButtonRecordInitial1Button);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecordInitial1Activity.this, RecordInitial2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
